b39f6172661761c48ac975713cbde543a725e392
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2023-12-16 08:15:19 +0400

    00001

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,13 @@
-const x: { name: string } = JSON.parse(`{"name": 3243242}`);
-console.log(x.name);
+let num1;
+let num2;
+
+console.log("before assignment", { num1: typeof num1, num2: typeof num2 });
+
+num1 = 5;
+num2 = "5.6";
+
+console.log("after assignment", { num1: typeof num1, num2: typeof num2 });
+
+const result = num1 + num2;
+
+console.log(result.toFixed());
