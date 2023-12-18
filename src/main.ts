4697fabc20e0e85c3a4600cb14cdc96d29e69988
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2023-12-18 14:13:22 +0400

    00006

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-const toInteger = (input: number): number => {
+let result;
+
+const toInteger = (input): number => {
   return Math.floor(input);
 };
 
-console.log(toInteger(123.456)); // Outputs: 123
-console.log(toInteger("123.456")); // Outputs: 123
+result = toInteger(123.456); // Outputs: 123
