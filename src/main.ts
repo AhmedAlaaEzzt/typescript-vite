fffc693fa2968b8d273d571f7e6714f03732b0f0
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2023-12-18 12:02:51 +0400

    00005

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
-const inputElement = document.getElementById("myInput");
+const toInteger = (input: number): number => {
+  return Math.floor(input);
+};
+
+console.log(toInteger(123.456)); // Outputs: 123
+console.log(toInteger("123.456")); // Outputs: 123
