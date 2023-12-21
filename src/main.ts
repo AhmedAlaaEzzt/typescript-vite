4b37f604831dd9105f779e323f7eb907fa8dde46
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2023-12-21 16:43:56 +0400

    00010

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log("hello world! typescript main file");
+// Processor 1: Convert to uppercase
+const toUpperCaseProcessor = (input: string) => input.toUpperCase();
+
+// Processor 2: Reverse the string
+const reverseStringProcessor = (input: string) =>
+  input.split("").reverse().join("");
+
+const stringInput = "Hello, World!";
+
+console.log(toUpperCaseProcessor(stringInput)); // Output: "HELLO, WORLD!"
+console.log(reverseStringProcessor(stringInput)); // Output: "!dlroW ,olleH"
