44d06273e1666c137b28e9605d98b08d8a3eefa7
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2023-12-22 20:44:11 +0400

    00013

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,51 @@ interface User {
   name: string;
   age: number;
   isActive: boolean;
+  email?: string;
 }
 
 const users: User[] = [
-  { id: 1, name: "Alice", age: 30, isActive: true, email: "alice@example.com" },
-  { id: 2, name: "Bob", age: 22, isActive: false },
-  { id: 3, name: "Carol", age: 33, isActive: true, email: "carol@example.com" },
+  {
+    id: 1,
+    name: "Alice",
+    age: 30,
+    isActive: true,
+    email: "alice@example.com",
+    displayInfo: function () {
+      console.log(
+        `User: ${this.name}, Age: ${this.age}, Email: ${
+          this.email || "No email"
+        }`
+      );
+    },
+  },
+  {
+    id: 2,
+    name: "Bob",
+    age: 22,
+    isActive: false,
+    displayInfo: function () {
+      console.log(
+        `User: ${this.name}, Age: ${this.age}, Email: ${
+          this.email || "No email"
+        }`
+      );
+    },
+  },
+  {
+    id: 3,
+    name: "Carol",
+    age: 33,
+    isActive: true,
+    email: "carol@example.com",
+    displayInfo: function () {
+      console.log(
+        `User: ${this.name}, Age: ${this.age}, Email: ${
+          this.email || "No email"
+        }`
+      );
+    },
+  },
 ];
 
-// Function to filter users with an email
-const usersWithEmail = users.filter((user) => user.email !== undefined);
-
-// Function to extract email addresses
-const userEmails = usersWithEmail.map((user) => user.email);
-
-console.log("Users with Email:", usersWithEmail);
-console.log("User Emails:", userEmails);
+users.forEach((user) => user.displayInfo());
