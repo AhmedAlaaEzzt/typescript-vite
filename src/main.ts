d72a0e2d56e05b9c3a38e1497ed9706ad55bdead
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2023-12-24 18:40:17 +0400

    00017

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 class BankAccount {
-  transactionCount = 0;
+  private _transactionCount = 0;
+
+  public get transactionCount() {
+    return this._transactionCount;
+  }
 }
 
 const myAccount = new BankAccount();
-myAccount.transactionCount = 100;
 
 console.log(myAccount);
