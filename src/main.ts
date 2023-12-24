ff5ec0cde8445a1bbbd660971d4a8096248f0220
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2023-12-24 18:55:33 +0400

    00018

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 class BankAccount {
-  private _transactionCount = 0;
+  private _transactionCount;
+
+  constructor(transactionCount: number) {
+    this._transactionCount = transactionCount;
+  }
 
   public get transactionCount() {
     return this._transactionCount;
   }
 }
 
-const myAccount = new BankAccount();
+const myAccount = new BankAccount(5);
 
 console.log(myAccount);
