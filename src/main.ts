70d54dde77dc15c7fee2056fd8b4e92ae8603d2e
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2023-12-25 14:28:42 +0400

    00021

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,20 @@ import { BankAccount } from "./BankAccount";
 
 const myAccount = new BankAccount("User Name", "00001", 1000, 1);
 
-myAccount.displayAccountInfo();
-myAccount.deposit(500);
-myAccount.displayAccountInfo();
-
 class SavingsAccount extends BankAccount {
   private interestRate: number; // Annual interest rate
+  constructor(
+    accountHolder: string,
+    accountNumber: string,
+    balance: number,
+    transactionCount: number,
+    interestRate: number
+  ) {
+    super(accountHolder, accountNumber, balance, transactionCount);
+    this.interestRate = interestRate;
+  }
 }
 
-const savingsAccount = new SavingsAccount("User Name 02", "00002", 100, 1);
+const savingsAccount = new SavingsAccount("User Name 02", "00002", 100, 1, 0.1);
+
+console.log(savingsAccount);
