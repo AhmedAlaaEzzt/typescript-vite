f3c89df9d6fd764a410331c074c9bee006cda410
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2023-12-25 14:21:08 +0400

    00020 add interestRate property

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ myAccount.displayAccountInfo();
 myAccount.deposit(500);
 myAccount.displayAccountInfo();
 
-class SavingsAccount extends BankAccount {}
+class SavingsAccount extends BankAccount {
+  private interestRate: number; // Annual interest rate
+}
 
 const savingsAccount = new SavingsAccount("User Name 02", "00002", 100, 1);
